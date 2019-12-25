b1c27c175877a06d5ee0846f2ef782d0d99a783c
Rick Copeland <rick@arborian.com>
2019-12-25 13:19:27 -0500

    Create new react app

---
+++ aliens-go-home/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>We will create an awesome game with React, Redux, and SVG!</h1>
+    </div>
+  );
+}
+
+export default App;
