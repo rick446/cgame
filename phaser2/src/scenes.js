cc4305c6c04fd63bcb104f8e74d0e82fb2cdb53a
Rick Copeland <rick@arborian.com>
2019-12-28 18:44:32 -0500

    Moving character

--- phaser2/src/scenes.js
+++ phaser2/src/scenes.js
@@ -1,37 +1,37 @@
+import fp from 'lodash/fp';
 import Phaser from 'phaser';
 
-export const BootScene = new Phaser.Class({
-    Extends: Phaser.Scene,
-
-    initialize: function BootScene() {
+export class BootScene extends Phaser.Scene {
+    constructor() {
+        super()
         Phaser.Scene.call(this, {key: 'BootScene' })
-    },
+    }
 
-    preload: function() {
+    preload() {
         this.load.image('tiles', 'assets/map/spritesheet.png');
         this.load.tilemapTiledJSON('map', 'assets/map/map.json');
         this.load.spritesheet('player', 'assets/RPG_assets.png', {
             frameWidth: 16, frameHeight: 16,
         });
-    },
+    }
 
-    create: function() {
+    create() {
         this.scene.start('WorldScene');
-    },
-});
+    }
+}
 
 
-export const WorldScene = new Phaser.Class({
-    Extends: Phaser.Scene,
+export class WorldScene extends Phaser.Scene {
 
-    initialize: function WorldScene() {
+    constructor() {
+        super()
         Phaser.Scene.call(this, {key: 'WorldScene' })
-    },
+    }
 
-    preload: function() {
-    },
+    preload() {
+    }
 
-    create: function() {
+    create = () => {
         let map = this.make.tilemap({key: 'map'});
 
         let tiles = map.addTilesetImage('spritesheet', 'tiles');
@@ -48,6 +48,66 @@ export const WorldScene = new Phaser.Class({
         this.player.setCollideWorldBounds(true);
 
         this.cursors = this.input.keyboard.createCursorKeys();
-    },
-});
 
+        this.cameras.main.setBounds(
+            0, 0, map.widthInPixels, map.heightInPixels
+        );
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.roundPixels = true;
+
+        let anims = {
+            left:  [1, 7, 1, 13],
+            right: [1, 7, 1, 13],
+            up:    [2, 8, 2, 14],
+            down:  [0, 6, 0, 12],
+        }
+        fp.flow([
+            fp.toPairs,
+            fp.forEach(
+                ([key, frames]) => {
+                    this.anims.create({
+                        key,
+                        frames: this.anims.generateFrameNumbers('player', {frames}),
+                        frameRate: 10,
+                        repeat: -1,
+                    })
+                }
+            )
+        ])(anims);
+
+        this.physics.add.collider(this.player, obstacles);
+    }
+
+    update() {
+        this.player.body.setVelocity(0);
+
+        // Movement
+        if(this.cursors.left.isDown) {
+            this.player.body.setVelocityX(-80);
+        } else if(this.cursors.right.isDown) {
+            this.player.body.setVelocityX(80);
+        }
+
+        if(this.cursors.up.isDown) {
+            this.player.body.setVelocityY(-80);
+        } else if(this.cursors.down.isDown) {
+            this.player.body.setVelocityY(80);
+        }
+
+        // Animation
+        if(this.cursors.left.isDown) {
+            this.player.anims.play('left', true);
+        } else if(this.cursors.right.isDown) {
+            this.player.anims.play('right', true);
+        } else if(this.cursors.up.isDown) {
+            this.player.anims.play('up', true);
+        } else if(this.cursors.down.isDown) {
+            this.player.anims.play('down', true);
+        } else {
+            this.player.anims.stop();
+        }
+
+
+
+    }
+}
